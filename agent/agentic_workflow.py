9617f612d58b3223814383368389a3d4bd2a86d6
Inwobodo <nwobodope@gmail.com>
2025-09-20 01:19:26 +0100

    code updated for loading the model

---
+++ agent/agentic_workflow.py
@@ -0,0 +1,28 @@
+
+from utils.model_loader import ModelLoader
+from prompt_library.prompt import SYSTEM_PROMPT
+from langraph.graph import StateGraph, MessageState, END, START
+from langraph.prebuilt import ToolNode, tools_condition
+
+#from tools.weather_info_tool import WeatherInfoTool
+#from tools.place_search_tool import PlaceSearchTool
+#from tools.calculator_tool import CalculatorTool
+#from tools.currency_convverter_tool import CurrencyConverterTool
+
+
+
+
+
+class GraphBuilder():
+    
+    def __init__(self):
+        pass
+    
+    def agent_function(self):
+        pass
+    
+    def build_graph(self):
+        pass
+    
+    def __call__(self):
+        pass
